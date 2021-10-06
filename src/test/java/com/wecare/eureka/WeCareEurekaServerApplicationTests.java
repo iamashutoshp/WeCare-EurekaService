56e12770e79a5053074f965356c96fbd50394238
iamashutoshp <ap1853177@gmail.com>
2021-10-06 20:21:35 +0530

    eureka server for WeCare first commit

---
+++ src/test/java/com/wecare/eureka/WeCareEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecare.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeCareEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
